35cf9423bcc28cefb91c20c6ff757760ec70e822
Akish tp <47858855+imakish@users.noreply.github.com>
2022-01-22 06:50:03 +0000

    Made API calls

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import os
 import discord
+import json
+import requests
 
-client=discord.Client()
+client = discord.Client()
+api_key = os.environ['API_KEY']
+
+
+def trending_movies():
+    response = requests.get(
+        "https://api.themoviedb.org/3/trending/all/day?api_key=" + api_key)
+    json_data = json.loads(response.text)
+    trending = json_data['results'][0]['original_title']
+    return (trending)
+
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('{0.user} is running. Catch him.'.format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  
-  msg = message.content
+    if message.author == client.user:
+        return
 
-  if msg.startswith('$hello'):
-    await message.channel.send("Hello")
+    msg = message.content
+
+    if msg.startswith('$hello'):
+        await message.channel.send("Hello")
+
+    if msg.startswith('$trending'):
+        movie = trending_movies()
+        await message.channel.send(movie)
+
 
 my_secret = os.environ['NOT_TOKEN']
 client.run(my_secret)
