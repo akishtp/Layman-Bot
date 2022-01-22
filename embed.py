587b1aea0cef22a2a7452fd8e74143916693c595
Akish tp <47858855+imakish@users.noreply.github.com>
2022-01-22 09:24:11 +0000

    gone back

--- embed.py
+++ embed.py
@@ -5,7 +5,7 @@ def trending_embed(ctx, title, description, image, rating):
 	embed.set_thumbnail(url = image)
 	for i in range(5):
 		embed.add_field(name=title[i]+"\t:star:"+str(rating[i]), value=description[i], inline=False)
-		embed.set_image(url=image)
+		#embed.set_image(url=image)
 	embed.set_footer(text="Information requested by {}".format(ctx.author.display_name))
 
 	return embed
