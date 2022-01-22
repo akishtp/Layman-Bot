ca24b04f95168c8cd6d4037f4ffc2e183b3b5a91
Akish tp <47858855+imakish@users.noreply.github.com>
2022-01-22 08:54:15 +0000

    Trending Embed works

--- embed.py
+++ embed.py
@@ -1,8 +1,8 @@
 import discord
 
-def embed(ctx, title, description, image):
-	embed = discord.Embed(title = "Top trending movies", url = "", description="These are the top trending movies right now", color = )
-	embed.set_thumbnail(url = image[0])
+def trending_embed(ctx, title, description, image):
+	embed = discord.Embed(title = "Top trending movies", url = "", description="These are the top trending movies right now", color = 0xA0DAA9 )
+	# embed.set_thumbnail(url = image[0])
 	for i in range(5):
 		embed.add_field(name=title[i], value=description[i], inline=False)
 	embed.set_footer(text="Information requested by {}".format(ctx.author.display_name))
--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import os
 from discord.ext import commands
 import json
 import requests
+from embed import *
 
 bot = commands.Bot(command_prefix="!")
 api_key = os.environ['API_KEY']
 
 
 def trending_movies():
-    movies=[]
+    title=[]
+    desc=[]
+    pic=[]
+    trending=[]
     response = requests.get(
         "https://api.themoviedb.org/3/trending/all/day?api_key=" + api_key)
     json_data = json.loads(response.text)
-    for i in range(2):
-      trending = json_data['results'][i]['title'or'name']
-      movies.append(trending)
-    return (movies)
+    for i in range(5):
+      try:
+        title.append(json_data['results'][i]['title'])
+      except:
+        title.append(json_data['results'][i]['name'])
+      desc.append(json_data['results'][i]['overview'])
+      pic.append(json_data['results'][i]['poster_path'])
+    trending.append(title)
+    trending.append(desc)
+    trending.append(pic)
+    return trending
 
 
 @bot.command(name="hello")
@@ -25,7 +36,7 @@ async def hello_world(ctx: commands.Context):
 @bot.command(name="trending")
 async def trending(ctx: commands.Context):
     movie = trending_movies()
-    await ctx.send(movie)
+    await ctx.send(embed=trending_embed(ctx,movie[0],movie[1],movie[2]))
 
 @bot.command(name="ping")
 async def ping(ctx):
