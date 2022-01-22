69997efb9d60e396d284900f60b393e91715a8c4
Arish <arish1248@gmail.com>
2022-01-22 14:29:06 +0530

    added rating

--- embed.py
+++ embed.py
@@ -1,10 +1,10 @@
 import discord
 
-def trending_embed(ctx, title, description, image):
+def trending_embed(ctx, title, description, image, rating):
 	embed = discord.Embed(title = "Top trending movies", url = "", description="These are the top trending movies right now", color = 0xA0DAA9 )
 	# embed.set_thumbnail(url = image[0])
 	for i in range(5):
-		embed.add_field(name=title[i], value=description[i], inline=False)
+		embed.add_field(name=title[i]+"\t"+rating[i], value=description[i], inline=False)
 	embed.set_footer(text="Information requested by {}".format(ctx.author.display_name))
 
 	return embed
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def trending_movies():
     desc=[]
     pic=[]
     trending=[]
+    rating = []
     response = requests.get(
         "https://api.themoviedb.org/3/trending/all/day?api_key=" + api_key)
     json_data = json.loads(response.text)
@@ -23,9 +24,8 @@ def trending_movies():
         title.append(json_data['results'][i]['name'])
       desc.append(json_data['results'][i]['overview'])
       pic.append(json_data['results'][i]['poster_path'])
-    trending.append(title)
-    trending.append(desc)
-    trending.append(pic)
+      rating.append(json_data['results'][i]['vote_average'])
+    trending = [title, desc, pic, rating]
     return trending
 
 
@@ -36,7 +36,7 @@ async def hello_world(ctx: commands.Context):
 @bot.command(name="trending")
 async def trending(ctx: commands.Context):
     movie = trending_movies()
-    await ctx.send(embed=trending_embed(ctx,movie[0],movie[1],movie[2]))
+    await ctx.send(embed=trending_embed(ctx,movie[0],movie[1],movie[2], movie[3]))
 
 @bot.command(name="ping")
 async def ping(ctx):
