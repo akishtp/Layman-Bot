3fe6c606bd31f0b6ac5c15a85b8746e4c4b0c602
imakish <makishtp@gmail.com>
2022-01-22 00:45:02 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import discord
+
+client=discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  msg = message.content
+
+  if msg.startswith('$hello'):
+    await message.channel.send("Hello")
+
+my_secret = os.environ['NOT_TOKEN']
+client.run(my_secret)
